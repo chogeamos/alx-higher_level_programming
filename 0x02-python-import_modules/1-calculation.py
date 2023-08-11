8605b63f1f8dc1fb0fe2f276f01c3f853fa36356
chogeamos <chogeamos@gmail.com.com>
2023-08-11 21:14:21 +0000

    hello

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,4 +1,4 @@
-#!/bin/bash/python3
+#!/usr/bin/python3
 
 if __name__ == "__main__":
     """Print the sum, difference, multiple and quotient of 10 and 5."""
@@ -11,4 +11,3 @@ if __name__ == "__main__":
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
     print("{} / {} = {}".format(a, b, div(a, b)))
-
