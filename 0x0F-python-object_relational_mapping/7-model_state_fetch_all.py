ab4a62ce66a99db888d1e19de46adeb87a57b702
chogeamos <chogeamos@gmail.com>
2024-01-12 12:11:21 -0800

    helloworld

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Start link class to table in database
+"""
+Lists all State objects from the database hbtn_0e_6_usa
 """
 import sys
 from model_state import Base, State
@@ -7,19 +8,23 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
-    # Create a database engine
+    # Check if the correct number of arguments is provided
+    if len(sys.argv) != 4:
+        print("Usage: {} <mysql_username> <mysql_password> <database_name>".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Creating a database engine
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
                            .format(sys.argv[1], sys.argv[2], sys.argv[3]))
 
-    # Create the tables in the database
+    # Creating the tables in the database
     Base.metadata.create_all(engine)
 
-    # Create a session
+    # Creating a session
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    # Query and print State instances ordered by id
-    for instance in session.query(State).order_by(State.id):
-        print(instance.id, instance.name, sep=": ")
-
+    # Query and print all State objects in ascending order by states.id
+    for state in session.query(State).order_by(State.id):
+        print("{}: {}".format(state.id, state.name))
 
